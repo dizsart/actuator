86c3ffac3e9c24c9abee3918b830f656dd8548a4
dizsart <darogadibia@gmail.com>
2020-06-26 22:40:24 +0100

    make sample rest controller

--- src/main/java/com/spring/actuator/control/SampleRestController.java
+++ src/main/java/com/spring/actuator/control/SampleRestController.java
@@ -1,4 +1,14 @@
 package com.spring.actuator.control;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SampleRestController {
+
+    @GetMapping
+    public String sample() {
+        return "Hello sample endpoint";
+    }
 }
